4d456a4dde978d642f87e9737601897915825ccc
Dave Bettin <dave.bettin@g5platform.com>
2011-03-31 10:59:49 -0700

    o require explicit path

--- lib/modelish.rb
+++ lib/modelish.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.expand_path('modelish',File.dirname(__FILE__)))
 
-require 'version'
-require 'base'
+require 'modelish/version'
+require 'modelish/base'
 
 module Modelish
   # Your code goes here...
